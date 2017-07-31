8378bab3141775c263976828e84f3f0f1fbdab80
Ines Chami <ineschami@hotmail.com>
2017-07-30 21:54:59 -0700

    added config file

---
+++ config.py
@@ -0,0 +1,10 @@
+# ************************************* CONFIG PARAMETERS *************************************
+batch_size = 1
+num_triplets = 2
+embedding_dim = 512
+hidden_dim = 100
+feat_map_dim = 14
+feat_map_depth = 512
+input_dim = 224
+upsampling_factor = input_dim / feat_map_dim
+epochs = 5
