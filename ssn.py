77f7887b1a0e08cae462497bf73389d573898ffd
Ines Chami <chami@tibet6.stanford.edu>
2017-10-06 17:51:47 -0700

    added dense layer for attention map transformation

--- ssn.py
+++ ssn.py
@@ -4,10 +4,10 @@
 from keras import backend as K
 from keras.applications.resnet50 import ResNet50
 from keras.layers import Dense, Flatten, UpSampling2D, Input, Activation
-from keras.layers.convolutional import Conv2DTranspose
-from keras.layers.core import Lambda, Dropout
+from keras.layers.convolutional import Conv2DTranspose, Conv2D
+from keras.layers.core import Lambda, Dropout, Reshape
 from keras.layers.embeddings import Embedding
-from keras.layers.merge import Multiply
+from keras.layers.merge import Multiply, Dot
 from keras.models import Model
 
 from config import parse_args
@@ -45,17 +45,15 @@ class ReferringRelationshipsModel():
         input_pred = Input(shape=(1,))
         input_obj = Input(shape=(1,))
         im_features = self.build_image_model(input_im)
-        obj_subj_embedding = self.build_embedding_layer(self.num_objects)
-        predicate_embedding = self.build_embedding_layer(self.num_predicates)
+        obj_subj_embedding = self.build_embedding_layer(self.num_objects, self.hidden_dim)
+        predicate_embedding = self.build_embedding_layer(self.num_predicates, (self.feat_map_dim)**4)
         embedded_subject = obj_subj_embedding(input_subj)
-        dense_subject = Dense(self.hidden_dim, activation="relu")(embedded_subject)
-        embedded_predicate= predicate_embedding(input_pred)
-        dense_predicate = Dense(self.hidden_dim, activation="relu")(embedded_predicate)
+        embedded_predicate = predicate_embedding(input_pred)
         embedded_object = obj_subj_embedding(input_obj)
-        dense_object = Dense(self.hidden_dim, activation="relu")(embedded_object)
-        subject_att = self.build_attention_layer(im_features, dense_subject)
-        predicate_att = self.build_attention_layer(subject_att, dense_predicate)
-        object_att = self.build_attention_layer(predicate_att, dense_object)
+        subject_att = self.build_attention_layer(im_features, embedded_subject)
+        predicate_att = self.build_map_transform_layer_dense(subject_att, embedded_predicate)
+        predicate_map = Multiply()([im_features, predicate_att])
+        object_att = self.build_attention_layer(predicate_map, embedded_object)
         subject_regions = self.build_upsampling_layer(subject_att, "subject_att")
         subject_regions_flat = Flatten(name="subject")(subject_regions)
         object_regions = self.build_upsampling_layer(object_att, "object_att")
@@ -83,16 +81,30 @@ class ReferringRelationshipsModel():
         im_features = image_branch(input_im)
         return im_features
 
-    def build_embedding_layer(self, num_categories):
-        return Embedding(num_categories, self.embedding_dim, input_length=1)
+    def build_map_transform_layer_dense(self, att_weights, pred_features):
+        att_weights_flat = Reshape((1, 196))(att_weights) # N x H
+        pred_matrix = Reshape((self.feat_map_dim**2, self.feat_map_dim**2))(pred_features) # H x H
+        att_transformed = Multiply()([att_weights_flat, pred_matrix])
+        att_transformed = Lambda(lambda x: K.sum(x, axis=2))(att_transformed)
+        att_transformed = Activation('sigmoid')(att_transformed)
+        att_transformed = Reshape((self.feat_map_dim, self.feat_map_dim, 1))(att_transformed)
+        return att_transformed
+    
+    def build_map_transform_layer_conv(self, att_weights, query):
+        conv_map = Conv2D(self.hidden_dim, 3, padding='same')(att_weights)
+        att_transformed = Multiply()([conv_map, query])
+        att_transformed = Lambda(lambda x: K.sum(x, axis=3, keepdims=True))(att_transformed)
+        att_transformed = Activation('sigmoid')(att_transformed)
+        return att_transformed
+
+    def build_embedding_layer(self, num_categories, emb_dim):
+        return Embedding(num_categories, emb_dim, input_length=1)
 
     def build_attention_layer(self, feature_map, query):
-        #dense_map = Dense(self.hidden_dim, activation="relu")(feature_map)
         attention_weights = Multiply()([feature_map, query])
         attention_weights = Lambda(lambda x: K.sum(x, axis=3, keepdims=True))(attention_weights)
         attention_weights = Activation('sigmoid')(attention_weights)
-        attended_map = Multiply()([feature_map, attention_weights])
-        return attended_map
+        return attention_weights
 
     def build_frac_strided_transposed_conv_layer(self, conv_layer):
         res = UpSampling2D(size=(2, 2))(conv_layer)
