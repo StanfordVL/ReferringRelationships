65adb81a390d9423d74a9b5c2da80f8ef80dae41
Ines Chami <chami@tibet6.stanford.edu>
2017-10-07 08:34:42 -0700

    fixed verbose

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python train.py --use-models-dir --lr 0.0001 --epochs 50 --models-dir /data/chami/ReferringRelationships/models/10_06_2017 --embedding-dim 256 --heatmap-threshold 0.5 --hidden-dim 2048 --feat-map-dim 7 --model ssn
+CUDA_VISIBLE_DEVICES=0 python train.py --use-models-dir --lr 0.0001 --epochs 50 --models-dir /data/chami/ReferringRelationships/models/10_06_2017 --embedding-dim 256 --heatmap-threshold 0.5 --hidden-dim 2048 --feat-map-dim 7 --model ssn --train-data-dir /data/chami/VRD/overfit/val/ --val-data-dir /data/chami/VRD/overfit/val/ --batch-size 8
