9ff96bc1800365818157f2c5144830ecd93852cc
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-10-13 12:26:52 -0700

    updating scripts

--- scripts/create_clevr_small.sh
+++ scripts/create_clevr_small.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python data.py --save-dir data/dataset-clevr-small --img-dir /data/ranjaykrishna/clevr/images/val --test --image-metadata data/clevr/test_image_metadata.json --annotations data/clevr/annotations_test.json --num-images 1000
-python data.py --save-dir data/dataset-clevr-small --img-dir /data/ranjaykrishna/clevr/images/train --image-metadata data/clevr/train_image_metadata.json --annotations data/clevr/annotations_train.json --num-images 10000
+python data.py --save-dir data/dataset-clevr-small --img-dir /data/ranjaykrishna/clevr/images/val --test --image-metadata data/clevr/test_image_metadata.json --annotations data/clevr/annotations_test.json --num-images 1000 --save-images
+python data.py --save-dir data/dataset-clevr-small --img-dir /data/ranjaykrishna/clevr/images/train --image-metadata data/clevr/train_image_metadata.json --annotations data/clevr/annotations_train.json --num-images 10000 --save-images --val-percent 0.3
