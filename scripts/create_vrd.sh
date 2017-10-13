021dc98742deed1e4f8a4550123e621de7281104
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-10-12 20:24:17 -0700

    fixes in errors

--- scripts/create_vrd.sh
+++ scripts/create_vrd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python data.py --save-dir data/dataset-vrd --img-dir /data/ranjaykrishna/vrd/images/val --test --image-metadata data/vrd/test_image_metadata.json --annotations data/vrd/annotations_test.json
-python data.py --save-dir data/dataset-vrd --img-dir /data/ranjaykrishna/vrd/images/train --image-metadata data/vrd/train_image_metadata.json --annotations data/vrd/annotations_train.json
+python data.py --save-dir data/dataset-vrd --img-dir /data/chami/VRD/sg_dataset/sg_test_images --test --image-metadata data/VRD/test_image_metadata.json --annotations data/VRD/annotations_test.json
+python data.py --save-dir data/dataset-vrd --img-dir /data/chami/VRD/sg_dataset/sg_train_images --image-metadata data/VRD/train_image_metadata.json --annotations data/VRD/annotations_train.json
