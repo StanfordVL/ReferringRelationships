476afb9632d5aa7c2f019a1d62bad1235a6bb6ab
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-10-13 20:42:42 -0700

    adding predicate iterator

--- iterator.py
+++ iterator.py
@@ -6,9 +6,117 @@ from keras.utils import Sequence
 
 import argparse
 import h5py
-import os
 import keras.backend as K
+import math
 import numpy as np
+import os
+
+
+class PredicateIterator(Sequence):
+    """Predicate iterator that corresponds to `data.PredicateDataset`.
+    """
+
+    def __init__(self, data_dir, args):
+        """Constructor for the iterator.
+
+        Args:
+            data_dir: Location of the annotations.
+            args: The arguments from the `config.py` file.
+        """
+        self.data_dir = data_dir
+        self.input_dim = args.input_dim
+        self.use_subject = args.use_subject
+        self.use_predicate = args.use_predicate
+        self.use_object = args.use_object
+        self.batch_size = args.batch_size
+
+        # Set the sizes of targets and images.
+        self.target_size = args.input_dim * args.input_dim
+        self.image_shape = (args.input_dim, args.input_dim, 3)
+        self.data_format = K.set_image_data_format('channels_last')
+
+        # Load the dataset
+        dataset = h5py.File(os.path.join(self.data_dir, 'dataset.hdf5'), 'r')
+        self.samples = 0
+        self.group_keys = []
+        self.sample_list = []
+        self.length = 0
+        for i in dataset:
+            self.group_keys.append(i)
+            group = dataset[i]
+            categories = group['categories']
+            predicate_samples = categories.shape[0]
+            self.samples += predicate_samples
+            batches = math.ceil(float(predicate_samples) /  self.batch_size)
+            self.sample_list.append(batches)
+            self.length += batches
+
+    def __len__(self):
+        """The number of items in the dataset.
+
+        Returns:
+            The number of items in the dataset.
+        """
+        return self.length
+
+    def on_epoch_end(self):
+        return
+
+    def __getitem__(self, idx):
+        """Grab the next batch of data for training.
+
+        Args:
+            idx: The index between 0 and __len__().
+
+        Returns:
+            The next batch as a tuple containing two elements. The first element
+            is batch of inputs which contains the image, subject, predicate,
+            object. These inputs change depending on which inputs are training
+            with. The second element of the tuple contains the output masks we
+            want the model to predict.
+        """
+        if not hasattr(self, 'images'):
+            images = h5py.File(os.path.join(self.data_dir, 'images.hdf5'), 'r')
+            self.images = images['images']
+            self.dataset = h5py.File(os.path.join(self.data_dir, 'dataset.hdf5'), 'r')
+
+        # Figure out the indices.
+        for i, l in enumerate(self.sample_list):
+            if idx < i:
+                group_idx = self.group_keys[i]
+                start_idx = int(idx % l)
+                end_idx = min(self.sample_list[idx],
+                              start_idx + self.batch_size)
+                break
+            else:
+                idx -= l
+        if group_idx is None:
+            raise ValueError('Iterator index out of range: %d / %d' % (
+                idx, self.length))
+
+        # Create the batches.
+        batch_rel = self.dataset[group_idx]['categories'][start_idx:end_idx]
+        batch_s_regions = self.dataset[group_idx]['subject_locations'][
+            start_idx:end_idx].reshape(self.batch_size, self.target_size)
+        batch_o_regions = self.dataset[group_idx]['object_locations'][
+            start_idx:end_idx].reshape(self.batch_size, self.target_size)
+        image_indices = batch_rel[: , 3]
+        current_batch_size = end_idx - start_idx
+        batch_image = np.zeros((current_batch_size,) + self.image_shape,
+                               dtype=K.floatx())
+        for i, image_index in enumerate(batch_rel[:, 3]):
+            batch_image[i] = self.images[image_index]
+
+        # Choose the inputs based on the parts of the relationship we will use.
+        inputs = [batch_image]
+        if self.use_subject:
+            inputs.append(batch_rel[:, 0])
+        if self.use_predicate:
+            inputs.append(batch_rel[:, 1])
+        if self.use_object:
+            inputs.append(batch_rel[:, 2])
+        return inputs, [batch_s_regions, batch_o_regions]
+
 
 
 class SmartIterator(Sequence):
