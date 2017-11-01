300b0b04c22b48bfb734ef85b6aac26bbea5dc8b
chami <chami@tibet4.stanford.edu>
2017-11-01 13:14:20 -0700

    edits in launch scripts

--- launch.py
+++ launch.py
@@ -22,7 +22,7 @@ parser.add_argument('--categorical-predicate', action='store_true')
 parser.add_argument('--num-predicates', type=str, default='70')
 parser.add_argument('--num-objects', type=str, default='100')
 #parser.add_argument('--use-subject', type=int, default=1, help='1/0 indicating whether to use the subjects.')
-#parser.add_argument('--use-predicate', type=int, default=1, help='1/0 indicating whether to use the predicates.')
+parser.add_argument('--use-predicate', type=str, default='1', help='1/0 indicating whether to use the predicates.')
 #parser.add_argument('--use-object', type=int, default=1, help='1/0 indicating whether to use the objects.')
 args = parser.parse_args()
 
@@ -30,7 +30,7 @@ args = parser.parse_args()
 for _ in range(args.nruns):
     params = {
         'lr': '%.4f' % random.uniform(0.0001, 0.01),
-        'patience': np.random.choice([2, 3, 5]),
+        'patience': np.random.choice([3, 5]),
         'lr-reduce-rate': '%.1f' % random.uniform(0.5, 0.9),
         'dropout': '%.1f' % random.uniform(0.0, 0.5),
         'opt': np.random.choice(['rms','adam']), 
@@ -40,18 +40,19 @@ for _ in range(args.nruns):
         'feat-map-layer': 'activation_40',
         'feat-map-dim': 14,
         'nb-conv-im-map': np.random.choice([1, 2]),
-        'conv-im-kernel': np.random.choice([1, 2, 3]),
+        'conv-im-kernel': 1,
         'nb-conv-att-map': np.random.choice([1, 2, 3, 4]),
-        'conv-predicate-kernel': np.random.choice([3, 4, 5, 6, 7]),
+        'conv-predicate-kernel': np.random.choice([3, 5, 7]),
         'heatmap-threshold': 0.5,
+        'val-steps-per-epoch': 50,
         #'w1': 1.,
     }
     arguments = ' '.join(['--' + k + ' ' + str(params[k]) for k in params])
     train = 'CUDA_VISIBLE_DEVICES=' + args.gpu + ' python train.py --use-models-dir --model ' + args.model + ' --epochs ' + args.epochs + ' --workers ' + args.workers
-    train += ' --models-dir ' + args.models_dir + ' --save-best-only --train-data-dir ' + args.train_data_dir + ' --val-data-dir ' + args.val_data_dir + ' --test-data-dir ' + args.test_data_dir
+    train += ' --models-dir ' + args.models_dir + ' --train-data-dir ' + args.train_data_dir + ' --val-data-dir ' + args.val_data_dir + ' --test-data-dir ' + args.test_data_dir
     if args.categorical_predicate:
         train += ' --categorical-predicate'
-    train += ' --num-predicates ' + args.num_predicates + ' --num-objects ' + args.num_objects
+    train += ' --num-predicates ' + args.num_predicates + ' --num-objects ' + args.num_objects + ' --use-predicate ' + args.use_predicate
     train += ' ' + arguments
     print('\n' +'*'*89 + '\n')
     print(train)
