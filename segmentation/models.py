8fa99e82d4ca58c1559bc7e88489e6a8da0937e4
chami <chami@tibet5.stanford.edu>
2017-11-10 17:36:06 -0800

    models ready

--- segmentation/models.py
+++ segmentation/models.py
@@ -5,11 +5,13 @@ from config import parse_args
 from keras import backend as K
 from keras.applications.resnet50 import ResNet50
 from keras.layers import Dense, Flatten, UpSampling2D, Input, Activation, BatchNormalization, RepeatVector
+from keras.layers.convolutional import Conv2DTranspose, Conv2D
 from keras.layers.core import Lambda, Dropout, Reshape
 from keras.layers.embeddings import Embedding
 from keras.layers.merge import Multiply, Dot, Add, Concatenate
 from keras.models import Model
 
+import numpy as np
 
 class BaseModel(object):
     """Contains helper functions.
@@ -26,9 +28,10 @@ class BaseModel(object):
         self.hidden_dim = args.hidden_dim
         self.num_objects = args.num_objects
         self.dropout = args.dropout
+        self.num_classes = args.num_classes 
         self.feat_map_layer = args.feat_map_layer
 
-    def get_image_features(self, input_image):
+    def get_image_features(self, input_im):
         base_model = ResNet50(weights='imagenet',
                               include_top=False,
                               input_shape=(self.input_dim, self.input_dim, 3))
@@ -38,24 +41,26 @@ class BaseModel(object):
         image_branch = Model(inputs=base_model.input, outputs=output)
         im_features = image_branch(input_im)
         im_features = Dropout(self.dropout)(im_features)
+        im_features = Conv2D(self.hidden_dim, 1,
+                             strides=(1, 1), padding='same',
+                             activation='relu')(im_features)
+        im_features = Dropout(self.dropout)(im_features)
         return im_features
 
-    def attend(self, feature_map, query, attention_conv, name=None):
+    def attend(self, feature_map, query):
         query = Reshape((1, 1, self.hidden_dim,))(query)
         attention_weights = Multiply()([feature_map, query])
         attention_weights = Lambda(lambda x: K.sum(x, axis=3, keepdims=True))(attention_weights)
-        attention_weights = Activation("relu", name=name)(attention_weights)
+        attention_weights = Activation("relu")(attention_weights)
         return attention_weights
 
-    def upsample(self, res, name=None):
+    def upsample(self, res):
         upsampling_factor = self.input_dim / self.feat_map_dim
         k = int(np.log(upsampling_factor) / np.log(2))
         for i in range(k):
-            res = UpSampling2D(size=(2, 2), name=name+"-upsampling-{}".format(i))(res)
-            res = Conv2DTranspose(1, 3, padding='same', use_bias=False, name=name+"-convT-{}".format(i), activation="relu")(res)
-        res = Reshape((self.input_dim * self.input_dim,))(res)
-        predictions = Activation("tanh", name=name)(res)
-        return predictions
+            res = UpSampling2D(size=(2, 2))(res)
+            res = Conv2DTranspose(1, 3, padding='same', use_bias=False, activation="relu")(res)
+        return res
 
 
 class SemanticSegmentationModel(BaseModel):
@@ -65,12 +70,15 @@ class SemanticSegmentationModel(BaseModel):
     def build_model(self):
         input_image = Input(shape=(self.input_dim, self.input_dim, 3))
         image_features = self.get_image_features(input_image)
-        object_regions = self.upsample(image_features, name="object")
-        model = Model(inputs=[input_image], outputs=[object_regions])
+        # adding 1 for background class 
+        object_regions = [Dense(1, activation="relu")(image_features) for i in range(self.num_classes + 1)]
+        upsampled_regions = Concatenate(axis=3)([self.upsample(x) for x in object_regions])
+        output = Lambda(lambda x: K.argmax(x, axis=3))(upsampled_regions)
+        model = Model(inputs=input_image, outputs=output)
         return model
 
 
-class ClassSegmentationModel():
+class ClassSegmentationModel(BaseModel):
     """Semantic segmentation model give the category.
     """
 
@@ -78,9 +86,19 @@ class ClassSegmentationModel():
         input_image = Input(shape=(self.input_dim, self.input_dim, 3))
         input_object = Input(shape=(1,))
         image_features = self.get_image_features(input_image)
-        embed = Embedding(self.num_objects, self.hidden_dim, input_length=1)
+        embed = Embedding(self.num_classes+1, self.hidden_dim, input_length=1)
         embedded_object = embed(input_object)
-        object_att = self.attend(image_features, embedded_object, attention_conv, name='subject-att')
-        object_regions = self.upsample(object_att, name="object")
-        model = Model(inputs=[input_image, input_object], outputs=[object_regions])
+        embedded_object = Activation("relu")(embedded_object)
+        object_att = self.attend(image_features, embedded_object)
+        object_regions = self.upsample(object_att)
+        model = Model(inputs=[input_image, input_object], outputs=object_regions)
         return model
+
+if __name__ == "__main__":
+    args = parse_args()
+#    sem_segm = SemanticSegmentationModel(args)
+#    model = sem_segm.build_model()
+#    print(model.summary())
+    class_segm = ClassSegmentationModel(args)
+    model = class_segm.build_model()
+    print(model.summary())
