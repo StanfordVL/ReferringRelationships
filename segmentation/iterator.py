a627ecf84a845e732df949a668e6ec0d8cf16d67
chami <chami@tibet5.stanford.edu>
2017-11-11 20:02:47 -0800

    fixed fcnn bug

--- segmentation/iterator.py
+++ segmentation/iterator.py
@@ -27,7 +27,7 @@ class SemanticSegmentationIterator(Sequence):
         self.num_objects = args.num_objects
 
         # Set the sizes of targets and images.
-        self.target_size = (args.input_dim, args.input_dim, args.num_objects)
+        self.target_size = (args.input_dim, args.input_dim, args.num_objects+1)
         self.image_shape = (args.input_dim, args.input_dim, 3)
         self.data_format = K.set_image_data_format('channels_last')
 
@@ -85,7 +85,7 @@ class SemanticSegmentationIterator(Sequence):
 
         # Choose the inputs based on the parts of the relationship we will use.
         inputs = batch_image
-        outputs = batch_o_regions.reshape((current_batch_size,) + self.target_size)[:, :, :, 1:]
+        outputs = batch_o_regions.reshape((current_batch_size,) + self.target_size)
         return inputs, outputs
 
 
