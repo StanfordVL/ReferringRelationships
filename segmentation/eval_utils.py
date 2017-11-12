bab726d0bf85c4d2590857cccb1aae9405fd7453
Ranjay Krishna <ranjaykrishna@gmail.com>
2017-11-11 19:42:40 -0800

    changing shapres of loss functions

--- segmentation/eval_utils.py
+++ segmentation/eval_utils.py
@@ -10,8 +10,9 @@ def sparse_accuracy_ignoring_last_label(y_true, y_pred):
     nb_classes = K.int_shape(y_pred)[-1]
     y_pred = K.reshape(y_pred, (-1, nb_classes))
 
-    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)),
-                       nb_classes)
+    #y_true = K.one_hot(tf.to_int32(K.flatten(y_true)),
+    #                   nb_classes)
+    y_true = K.reshape(y_true, (-1, nb_classes))
     #unpacked = tf.unstack(y_true, axis=-1)
     legal_labels = ~tf.cast(y_true[-1], tf.bool)
     #y_true = tf.stack(unpacked[1:], axis=-1)
--- segmentation/train_utils.py
+++ segmentation/train_utils.py
@@ -32,12 +32,14 @@ def multinomial_logistic_loss(y_true, y_pred, eps=10e-8):
 
 
 def softmax_sparse_crossentropy_ignoring_last_label(y_true, y_pred):
-    y_pred = K.reshape(y_pred, (-1, K.int_shape(y_pred)[-1]))
+    nb_classes = K.int_shape(y_pred)[-1]
+    y_pred = K.reshape(y_pred, (-1, nb_classes))
     log_softmax = tf.nn.log_softmax(y_pred)
 
-    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), K.int_shape(y_pred)[-1])
+    #y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), K.int_shape(y_pred)[-1])
     #unpacked = tf.unstack(y_true, axis=-1)
     #y_true = tf.stack(unpacked[1:], axis=-1)
+    y_true = K.reshape(y_true, (-1, nb_classes))
 
     cross_entropy = -K.sum(y_true * log_softmax, axis=1)
     cross_entropy_mean = K.mean(cross_entropy)
