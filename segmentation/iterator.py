1434f98d26b6495995ab9534208354bcbf1ce538
chami <chami@tibet5.stanford.edu>
2017-11-11 19:19:17 -0800

    fixed batch size bug

--- segmentation/iterator.py
+++ segmentation/iterator.py
@@ -78,7 +78,7 @@ class SemanticSegmentationIterator(Sequence):
 
         start_idx = idx * self.batch_size
         end_idx = min(self.samples, (idx + 1) * self.batch_size)
-
+        current_batch_size = end_idx - start_idx
         # Create the batches.
         batch_o_regions = self.objects[start_idx:end_idx]
         batch_image = self.images[start_idx:end_idx]
