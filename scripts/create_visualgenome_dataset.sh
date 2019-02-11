bf5ea86558f5911ee9c16f37be8e0db71310cb84
Jaesun Park <diligensloth@gmail.com>
2019-02-11 15:14:34 +0900

    Fix path of Visual Genome data

--- scripts/create_visualgenome_dataset.sh
+++ scripts/create_visualgenome_dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 VISUAL_GENOME_IMAGES_DIR=$1
-python data.py --save-dir data/dataset-vrd --img-dir $VISUAL_GENOME_IMAGES_DIR --test --image-metadata data/VRD/test_image_metadata.json --annotations data/VRD/annotations_test.json --save-images
-python data.py --save-dir data/dataset-vrd --img-dir $VISUAL_GENOME_IMAGES_DIR --image-metadata data/VRD/train_image_metadata.json --annotations data/VRD/annotations_train.json --save-images
+python data.py --save-dir data/dataset-visualgenome --img-dir $VISUAL_GENOME_IMAGES_DIR --test --image-metadata data/VisualGenome/test_image_metadata.json --annotations data/VisualGenome/annotations_test.json --save-images
+python data.py --save-dir data/dataset-visualgenome --img-dir $VISUAL_GENOME_IMAGES_DIR --image-metadata data/VisualGenome/train_image_metadata.json --annotations data/VisualGenome/annotations_train.json --save-images
