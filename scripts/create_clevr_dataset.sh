61e04c1ccad6a7f7dcbc07562ef68546ba4063cf
Ranjay Krishna <ranjaykrishna@gmail.com>
2019-07-16 11:02:04 -0700

    Saving CLEVR images when building hdf5 dataset.

--- scripts/create_clevr_dataset.sh
+++ scripts/create_clevr_dataset.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 CLEVR_TRAIN_IMAGES_DIR=$1
 CLEVR_VAL_IMAGES_DIR=$2
-python data.py --save-dir data/dataset-clevr --img-dir $CLEVR_VAL_IMAGES_DIR --test --image-metadata data/clevr/test_image_metadata.json --annotations data/clevr/annotations_test.json
-python data.py --save-dir data/dataset-clevr --img-dir $CLEVR_TRAIN_IMAGES_DIR --image-metadata data/clevr/train_image_metadata.json --annotations data/clevr/annotations_train.json
+python data.py --save-dir data/dataset-clevr --img-dir $CLEVR_VAL_IMAGES_DIR --test --image-metadata data/clevr/test_image_metadata.json --annotations data/clevr/annotations_test.json --save-images
+python data.py --save-dir data/dataset-clevr --img-dir $CLEVR_TRAIN_IMAGES_DIR --image-metadata data/clevr/train_image_metadata.json --annotations data/clevr/annotations_train.json --save-images
